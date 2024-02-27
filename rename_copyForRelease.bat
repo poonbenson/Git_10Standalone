ed48be55372dd993a8de36966114eed3c4768f71
poonbenson <56299311+poonbenson@users.noreply.github.com>
2024-02-28 02:13:21 +0800

    20240228

--- rename_copyForRelease.bat
+++ rename_copyForRelease.bat
@@ -1,4 +1,5 @@
-ren bigKeeperTest_publish.py bigKeeperTest_standalone.py
+copy bigKeeperTest_publish.py bigKeeperTest_standalone.py
+del bigKeeperTest_publish.py
 copy bigKeeperTest_standalone.py ..\
 
-pause
+timeout 7
